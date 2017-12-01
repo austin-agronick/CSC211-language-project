64517a0580dfda81e43ab7f547154ab16cda85f5
Unknown <austin@my.uri.edu>
2017-12-01 14:53:14 -0500

    Initial language.h code
    Also renaming main.cpp to frequencies.cpp

---
+++ language.h
@@ -0,0 +1,41 @@
+#ifndef _language_h
+#define _language_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+/*
+A class to represent the the language data.
+A LANG object has one member field: the text, which is a string of characters.
+*/
+
+class LANG {
+
+public:
+
+	/*
+	Default constructor
+	*/
+	LANG();
+
+	/*
+	Constructor with argument for text of langauge data.
+	If `text` contains invalid characters, throws a std::runtime_error
+	*/
+	LANG(std::string text);
+
+	/*
+	Getter methods
+	*/
+	std::vector<int> getFrequency();
+
+private:
+
+	/* instance variables */
+	std::string text;
+
+};
+
+
+#endif
