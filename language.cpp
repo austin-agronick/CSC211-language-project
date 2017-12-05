6b6510af96217c3b7dccadcf454861e90676a5c9
Unknown <austin@my.uri.edu>
2017-12-04 23:53:03 -0500

    All code working and commented

--- language.cpp
+++ language.cpp
@@ -1,45 +1,44 @@
 #include "language.h"
-#include <algorithm> // std::find
 
 // Default Constructor
 LANG::LANG() {
   txt = ""; // Initialize global text variable to empty value
-
 }
 
 // Creates LANG object given the text string
 LANG::LANG(std::string text) {
-//    std::vector<char> abc = { ' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    // Checks if given input is valid
     for (unsigned long i = 0; i < text.size(); i++) {
         if (text[i] != '\n' && text[i] != ' ' && (text[i] < 'a' || text[i] > 'z')) {
             throw std::runtime_error("ERROR: Invalid text input, must only contain lowercase letters and spaces");
         }
     }
-
     txt = text; // Initialize global text variable
 }
 
-
+// getter for language text data
 std::string LANG::getText() {
     return txt;
 }
 
 /*
 get a frequency profile
-    -takes string of language data and starting at first index, incrementing by one and finding the trigram value for every 3 characters
-    -add one to the frequency counter vector at that trigram value index
+    - takes string of language data from a LANG object and starting at first index, increments by one while finding the trigram value for every 3 characters
+    - add one to the frequency counter vector at that trigram value index
 */
 std::vector<int> LANG::getFrequency() {
+  // initialize frequencies vector to 19683 zeroes
   std::vector<int> frequencies(19683);
-      // Start at the first three characters
-      for (unsigned long i = 0; i < txt.size()-2; i++) {
+      // iterate through text input,
+      for (unsigned long i = 0; i < txt.size()-2; i++)
+      {
+        // initialize variables for letters in the trigram
         char letter1 = txt[i];
         char letter2 = txt[i+1];
         char letter3 = txt[i+2];
 
-        if (letter1 =='\n') {
-          continue;
-        }
+        // check if the trigram contains a newline character, and if so skip over it
+        if (letter1 =='\n') continue;
         else if (letter2 =='\n') {
           letter2 = txt[i+2];
           letter3 = txt[i+3];
@@ -48,21 +47,19 @@ std::vector<int> LANG::getFrequency() {
           letter3 = txt[i+3];
         }
 
+        // get the value of each letter
         int a = letter1 - 'a' + 1;
         int b = letter2 - 'a' + 1;
         int c = letter3 - 'a' + 1;
+
         // check if trigram contains spaces
         if (letter1 == ' ') a = 0;
         if (letter2 == ' ') b = 0;
         if (letter3 == ' ') c = 0;
 
-        //calculate frequencies with formula
-        int index = ((a*(27*27))+(b*27)+c);
+        // find the index of the trigram by using the formula and add 1 to its index
+        int index = ((a*(27*27)) + (b*27) + c);
         frequencies[index]+=1;
-        }
-        for(unsigned long i=0;i < 19683;i++){
-          std::cout<<frequencies[i]<< ' ';
-        }
-        return frequencies;
-
+      }
+      return frequencies;
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "language.h"
 
 int main(int argc, char**argv) {
-    if(argc !=2)return 1;
+    // check if 2 arguments are passed
+    if(argc !=2) return 1;
+    // create LANG object a and get its frequency profile
     LANG a(argv[1]);
     a.getText();
     std::vector<int> output = a.getFrequency();
-
+    // output frequencies vector
+    for(unsigned long i=0; i < 19683; i++){
+      std::cout<<output[i]<<' ';
+    }
+    std::cout<<std::endl;
 }
