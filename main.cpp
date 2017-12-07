c9b2d72a41d1c10a66ab70d23267016a92c5bd81
Unknown <austin@my.uri.edu>
2017-12-06 22:04:26 -0500

    Fixed bug in main.cpp by implementing ifstream

--- main.cpp
+++ main.cpp
@@ -3,20 +3,22 @@
 int main(int argc, char**argv) {
     // check if 2 arguments are passed
     if(argc<3) return 1;
-    std::ifstream testFile(argv[argc-1]);
-    /*//create LANG obect for test data and fets its frequency profile
-    LANG testFile(argv[argc-1]);
+    //create LANG obect for test data and fets its frequency profile
+    // std::ifstream
+    std::ifstream lang(argv[argc]);
+    LANG testFile(lang);
     double mostSimiliar = 100;
     int mostSimiliarIndex = 0;
     for(int i=1; i<argc; i++){
       // create LANG object a and get its frequency profile
-      LANG fileX(argv[i]);
-      double similarity = fileX.calcSimilarity(testFile);
+      std::ifstream fileX(argv[i]);
+      LANG file(fileX);
+      double similarity = file.calcSimilarity(testFile);
       if (similarity < mostSimiliar){
         mostSimiliar = similarity;
         mostSimiliarIndex = i;
       }
     }
-    std::cout<<argv[mostSimiliarIndex]<<std::endl; */
+    std::cout<<argv[mostSimiliarIndex]<<std::endl;
     return 0;
 }
